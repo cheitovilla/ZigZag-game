81471d84df2b6434b9177d30cf3fc1e3c9843c91
Jaime Garcia <cheitovilla@outlook.com>
2019-01-24 10:07:36 -0500

    Working scripts, falta UI
    next steps: UI

---
+++ ZigZag Game/Assets/Scripts/ControllerCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerCamera : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 posIni;
+
+	// Use this for initialization
+	void Start ()
+    {
+        posIni = this.gameObject.transform.position - player.transform.position;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = player.transform.position + posIni;
+	}
+}
--- ZigZag Game/Assets/Scripts/Player.cs
+++ ZigZag Game/Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    public GameObject contCoins;
     public float force;
     public bool changeDir;
     private Vector3 dir;
@@ -21,9 +22,10 @@ public class Player : MonoBehaviour {
     {
         if (transform.position.y < -1)
         {
-            this.transform.position = new Vector3(4.5f, 0.65f, 4.25f);
-            rb.Sleep();
-            dir = new Vector3(0, 0, 0);
+            //this.transform.position = new Vector3(4.5f, 0.65f, 4.25f);
+            //rb.Sleep();
+            //dir = new Vector3(0, 0, 0);
+            RestartGame();
         }
 
         if (Input.GetMouseButtonDown(0))
@@ -45,5 +47,31 @@ public class Player : MonoBehaviour {
     private void FixedUpdate()
     {
         rb.MovePosition(transform.position + dir * Time.deltaTime * force);
+    }
+
+     void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Coin")
+        {
+            other.gameObject.SetActive(false);
+        }
+        if (other.gameObject.name == "Win")
+        {
+            RestartGame();
+        }
+    }
+
+    void RestartGame()
+    {
+        this.transform.position = new Vector3(4.5f, 0.65f, 4.25f);
+        rb.Sleep();
+        dir = new Vector3(0, 0, 0);
+
+        for (int i = 0; i < contCoins.transform.GetChildCount(); i++) //contCoins.transform.GetChildCount()
+        {
+            contCoins.transform.GetChild(i).gameObject.SetActive(true);
+            
+        }
+
     }
 }
